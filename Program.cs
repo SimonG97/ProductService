7094fe4ff8402d678560a2bafd977467e17cadce
Simon <simon.gurung@thimphutechpark.bt>
2022-12-21 15:36:45 +0600

    Repository added

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using ProductService;
+using ProductService.DbContexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//configuring database
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseNpgsql(connectionString));
+
+//configuring automapper
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 
 var app = builder.Build();
 
