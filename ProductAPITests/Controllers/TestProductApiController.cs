61af8c815f33034762404ec7df0cb776758bd3c6
Simon <simon.gurung@thimphutechpark.bt>
2023-03-29 17:03:18 +0600

    Configuring the unit test

--- ProductAPITests/Controllers/TestProductApiController.cs
+++ ProductAPITests/Controllers/TestProductApiController.cs
@@ -1,6 +1,7 @@
 using Moq;
 using ProductAPI.Dto;
 using ProductAPI.IRepository;
+using ProductAPITests.MockData;
 
 namespace ProductAPITests.Controllers;
 
@@ -8,11 +9,17 @@ public class TestProductApiController
 {
     private protected ResponseDto _response;
     private readonly Mock<IProductRepository> _productRepository;
+
+    public TestProductApiController(ResponseDto response, Mock<IProductRepository> productRepository)
+    {
+        _response = response;
+        _productRepository = productRepository;
+    }
+
     [Fact]
     public async Task Get_ShouldReturnIsSuccessTrue()
     {
         //Arrange
-
 
         //Act
 
